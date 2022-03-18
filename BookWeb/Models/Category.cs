b6ef0d8df8ad9e52a21b67b053165c7e5dccafea
Monster <Monster@MONSTER>
2022-03-18 09:08:21 +0300

    Add project files.

---
+++ BookWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookWeb.Models
+{
+    public class Category
+    {
+        //prop tab
+        [Key] //primary key
+        public int Id { get; set; }
+        [Required]
+        public string Name{ get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,1000,ErrorMessage="Display Order must be between 1 and 100 only!")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime{ get; set; } = DateTime.Now;
+    }
+}
---
+++ BookWeb/Program.cs
@@ -0,0 +1,39 @@
+using BookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+//links
+//https://www.youtube.com/watch?v=hZ1DASYd9rk
+//https://github.com/CodeSeven/toastr
+//https://bootswatch.com/quartz/?optionsRadios=option1
+//https://getbootstrap.com/docs/5.1/getting-started/introduction/
